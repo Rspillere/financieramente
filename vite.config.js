cc565f90c7766deade65ee4ff06015d6046dbb70
Rspillere <rspillere@gmail.com>
2024-07-23 17:03:13 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://financieramente.com.uy/',
+  base: 'https://rspillere.github.io/financieramente/',
   plugins: [react()],
   
   
